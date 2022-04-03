60c1ea996ef64c1cf73c9dcade69e6adec359b5d
sjrojanooo <sjrojano@gmail.com>
2022-04-03 13:30:37 -0700

    edits on some comments in main file

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from datetime import datetime # datetime to time the duration of the script; 
-from scrape_test import scrape_main; 
+import time; # set a timeout when running the application; 
+from scrape_test import scrape_main; # importing my scraping and data manipulation py file
+from gmail_api import gmail_main; # importing gmail api extract and load method; 
+
 
 def main():
 
+    # performs query, downloads attachment; 
+    # Extract, and partly load phase for the html document; 
+    gmail_main(); 
+
+    # so the newest document in the html directory get read I added a sleep of 3 seconds; 
+    time.sleep(3)
+
+    # Transform and load phase - for the excel files; 
     scrape_main(); 
     
-    pass; 
-
 if __name__ == "__main__":
 
     t1 = datetime.now()
@@ -17,6 +26,6 @@ if __name__ == "__main__":
 
     totalTime = t1 - t2; 
 
-    print(f"Program it took {totalTime}")
+    print(f"Program took {totalTime}")
 
     
