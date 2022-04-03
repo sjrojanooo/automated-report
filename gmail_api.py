95cc85a862cd5105d4d5f22331741f0147384d1f
sjrojanooo <sjrojano@gmail.com>
2022-04-03 16:27:34 -0700

    complete message and body preparation for message

--- gmail_api.py
+++ gmail_api.py
@@ -12,6 +12,8 @@ from io import BytesIO; # allows us to stream file in memory;
 import base64; # handles encoding and decoding of binary objects; 
 from bs4 import BeautifulSoup; # to parse the htm documents table;
 from dotenv import dotenv_values; # loading all dotenv variables; 
+import pandas as pd; # pandas library; 
+import datetime; # get date
 
 # creating a dictionary from all the variables located in my .env file; 
 config = {**dotenv_values('.env')}
@@ -23,7 +25,7 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleap
 # global variable that holds the email address 
 foxy_email_address = config["FOXY_PRODUCE_EMAIL"]; 
 
-def gmail_main():
+def gmail_extract_load():
 
     try:
         # Call the Gmail API
@@ -51,10 +53,14 @@ def gmail_main():
 
 
 
+
 def get_credentials():
 
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
+    """
+    This is a GMAIL API template that can be found on the GMAIL API official documentation. 
+    The original source was meant to get the list of mailbox labels from the intended users account. 
+    I am implementing my own use case to automate some reporting for work. I removed the label fetch, 
+    and am just using the credential authenticator part of the script to authorize access to my account.
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -76,6 +82,9 @@ def get_credentials():
 
     return creds; 
 
+
+
+# queries the email by the senders email address; 
 def query_foxy_product(service: object):
 
         results = service.users()\
@@ -107,6 +116,7 @@ def get_attachment_id(service:object, results:object, msg: object):
                                 .execute()
         return att; 
 
+# decodes the captured attachment; 
 def beautiful_soup_parsing(attachment: object):
 
         # processing the binary object and streaming it in memory. 
@@ -118,11 +128,69 @@ def beautiful_soup_parsing(attachment: object):
 
         return soup; 
 
-def write_object_to_file(soup: str):
+# writes the beautiful soup object to the designated directory; 
+def write_object_to_file(soup: object):
 
     # wrtiting the file to the local directory; 
     with open("./data/html-doc/cooler-report.htm", "w", encoding = 'utf-8') as file:
     
         # prettify the soup object and convert it into a string  
         file.write(str(soup.prettify())); 
+
+
+# walks directory to see which new files were created, splits by delimiter and retrieved the area for each file 
+# and returns a list; 
+def walk_data_excel_dir():
+    
+    area_list = []; 
+
+    for root, dir_names ,fileNames in os.walk(os.path.abspath('')):
+        
+        for f in fileNames: 
+
+            if f.endswith('.xlsx'):
+
+                area_list.append(f.split('-')[0])
+
+    print(area_list)
+
+    return area_list; 
+
+
+# prepares a the subject and body of the message conditionally by the len of the area_list obtained 
+# after walking the directory; 
+def prepare_mail(area_list: list):
+    
+    subject_message = None; 
+
+    body_message = None; 
+
+    date = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+
+    day_name = pd.to_datetime(date).day_name()
+
+    if len(area_list) ==3: 
+
+        subject_message = f'{area_list[0]}, {area_list[1]}, {area_list[2]} - HM - Cooler Report - {day_name} - {date}';
+
+        body_message = f'Good Morning,\nAttached are the Harvest Management Cooler Reports from {area_list[0]}, {area_list[1]}, and {area_list[2]} for {date}. Have a great day everyone';
+
+        return subject_message;   
+
+    elif len(area_list) ==2: 
+
+        subject_message = f'{area_list[0]}, {area_list[1]} - HM - Cooler Report - {day_name} - {date}';
+
+        body_message = f'Good Morning,\nAttached are the Harvest Management Cooler Reports from {area_list[0]}, {area_list[1]}, and {area_list[2]} for {date}. Have a great day everyone';
+
+        return subject_message;    
+
+    elif len(area_list) ==1: 
+
+        subject_message = f'{area_list[0]} - HM - Cooler Report - {day_name} - {date}';
+
+        body_message = f'Good Morning,\nAttached are the Harvest Management Cooler Reports from {area_list[0]}, {area_list[1]}, and {area_list[2]} for {date}. Have a great day everyone';
+
+        return subject_message;
+
 
