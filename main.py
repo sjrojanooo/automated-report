9a38f3a394b644b4fe9ab5a1959f12b620634a9e
sjrojanooo <sjrojano@gmail.com>
2022-04-03 16:33:16 -0700

    changed method name

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from datetime import datetime # datetime to time the duration of the script; 
 import time; # set a timeout when running the application; 
 from scrape_test import scrape_main; # importing my scraping and data manipulation py file
-from gmail_api import gmail_main; # importing gmail api extract and load method; 
+from gmail_api import gmail_extract_load; # importing gmail api extract and load method; 
 
 
 def main():
 
     # performs query, downloads attachment; 
     # Extract, and partly load phase for the html document; 
-    gmail_main(); 
+    gmail_extract_load(); 
 
     # so the newest document in the html directory get read I added a sleep of 3 seconds; 
     time.sleep(3)
