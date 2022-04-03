7dc496192de9cb72852f63060c38d58a403b3d6f
sjrojanooo <sjrojano@gmail.com>
2022-04-03 13:30:59 -0700

    edit on some comments for scrape main

--- scrape_test.py
+++ scrape_test.py
@@ -1,7 +1,5 @@
-from datetime import datetime # datetime to time the duration of the script; 
 from bs4 import BeautifulSoup; # to parse the htm documents table;
 from collections import defaultdict; # to create a dictrionary 
-from io import StringIO # to stream from in memory instead of using up disk space; 
 import pandas as pd; # since its a small data set I decided to use pandas for the data manipulation; 
 
 def scrape_main(): 
@@ -10,23 +8,32 @@ def scrape_main():
     box_labels_df = read_json("./data/box-labels/box_labels.json"); 
 
     # creates a dictionary based of of the parsed information from the htm document; 
-    cooler_dict = create_dictionary(soup_table_rows("./data/html-doc/cooler-report.htm")); 
+    rows, report_date = soup_table_rows_and_date("./data/html-doc/cooler-report.htm")
+
+    # converting the report date to the day name; 
+    report_day_name = get_report_day_name(report_date)
+
+    # creating ragged dictionary; 
+    cooler_dict = create_dictionary(rows); 
 
     # creating the final data frame of the product reported from cooler system; 
-    final_df = create_final_frame(create_frame(cooler_dict["lot_id"], ["vendor","lot_id"]),
+    final_df = create_final_frame(
+                create_frame(cooler_dict["lot_id"], ["vendor","lot_id"]),
                 create_frame(cooler_dict["ranch"], ["vendor", "ranch"]),
                 create_frame(cooler_dict["item_label"], ["vendor","item_label"]),
                 create_frame(cooler_dict["item_name"], ["vendor","item_name"]),
-                create_frame(cooler_dict["quantity"], ["vendor","quantity"])); 
+                create_frame(cooler_dict["quantity"], ["vendor","quantity"])
+                ); 
 
-    area_list = get_unique_count_of_area(final_df);
+    # getting the area list obtained 
+    area_list = get_unique_list_of_area(final_df);
 
+    # creating all necessary dataframe 
     dataFrame = conditional_frame(final_df, box_labels_df, area_list);
 
-    # creating excel files; 
-    condition_excel_files(dataFrame, area_list)
-
-    print(dataFrame[0])
+    # creating excel files - day name is also appended to them; 
+    # files will be written out to the gmail-python/data/excel-doc path point; 
+    condition_excel_files(dataFrame, area_list,report_day_name)
 
 # current box labels in use; 
 def read_json(filePath: str):
@@ -36,15 +43,27 @@ def read_json(filePath: str):
     return jsonDf; 
 
 # creates beautiful soup instance and return all rows; 
-def soup_table_rows(filePath: str): 
+def soup_table_rows_and_date(filePath: str): 
     
     soup = BeautifulSoup(open(filePath,"r+"), "lxml"); 
 
-    return soup.find_all("tr"); 
+    rows = soup.find_all("tr"); 
 
-# creates a dictionary after html table scrap is complete; 
-# the report has a ragged structure to it, and since its automatically generated there are 
-# no class types for tables or rows. 
+    date = soup.find("td").text.strip()
+    
+    return rows, date; 
+
+def get_report_day_name(date: str):
+
+    report_day_name = pd.to_datetime(date).day_name(); 
+
+    return report_day_name
+
+"""
+    creates a dictionary after html table scrap is complete; 
+    the report has a ragged structure to it, and since its automatically generated there are 
+    no class types for tables or rows. 
+"""
 def create_dictionary(rows: object):
 
     report_dict = defaultdict(list);
@@ -127,9 +146,11 @@ def create_area_column(dataFrame: object, lot_id_column: str, area_column: str):
                                 .astype(str)\
                                     .str.replace(r'''\w{1}\d+''','', regex=True)
 
-    # filtering method to set all areas by the alhpabetic character from the replace method above; 
-    # figured out the A & B stand for Huron A in this case is Fall and B is for Spring; 
-    # Y and S are for Yuma and Salinas respectively; 
+    """
+    filtering method to set all areas by the alhpabetic character from the replace method above; 
+    figured out the A & B stand for Huron A in this case is Fall and B is for Spring; 
+    Y and S are for Yuma and Salinas respectively; 
+    """
     dataFrame.loc[((dataFrame[area_column] == 'A') | (dataFrame[area_column] == 'B')), area_column] = 'Huron'
     dataFrame.loc[dataFrame[area_column] == 'S', area_column] = "Salinas"
     dataFrame.loc[dataFrame[area_column] == 'Y', area_column] = "Yuma"
@@ -147,7 +168,7 @@ def remove_delimeter_from_int(dataFrame: object, quantity_column: str):
     return dataFrame
 
 # getting the unique count of areas that reported product; 
-def get_unique_count_of_area(dataFrame: object): 
+def get_unique_list_of_area(dataFrame: object): 
 
     area_list = sorted(dataFrame['area'].unique()); 
 
@@ -171,77 +192,62 @@ def conditional_frame(dataFrame: object, boxDataFrame, area_list: list):
             return join_data_by_area(dataFrame,boxDataFrame, area_list[0]), join_data_by_area(dataFrame,boxDataFrame, area_list[1]), join_data_by_area(dataFrame,boxDataFrame, area_list[2])
     except: 
 
-        print("ERROR HAPPENED AT CONDITIONAL FRAME!!!")
-
+        raise print("error happened at conditional_frame method")
+        
+    
 # returning the column name on which I will be performing the join on; 
 def return_box_label_columns(area: str):
 
-    try: 
+    billing_center = None; 
 
-        billing_center = None; 
+    if area == 'Salinas':
+        billing_center = 'ca_cost_center'
+    elif area == 'Yuma':
+        billing_center = 'az_cost_center'
+    else: 
+        billing_center = 'hu_cost_center'
 
-        if area == 'Salinas':
-            billing_center = 'ca_cost_center'
-        elif area == 'Yuma':
-            billing_center = 'az_cost_center'
-        else: 
-            billing_center = 'hu_cost_center'
+    columns = ['item_label','commodity',billing_center]; 
 
-        columns = ['item_label','commodity',billing_center]; 
+    return columns; 
 
-        return columns; 
-    
-    except: 
-
-        print("ERROR HAPPENED AT return_box_labels_columns"); 
-
-# using a left merge which is similary to left join in sql to combine on the item lable 
-# relationship with the area. It uses this, because there are cases in which the automated processing system from 
-# the cooler creates a new label that we do not have saved on our end. I leave the field as an empty space instead; 
+"""
+    using a left merge which is similary to left join in sql to combine on the item lable 
+    relationship with the area. It uses this, because there are cases in which the automated processing system from 
+    the cooler creates a new label that we do not have saved on our end. I leave the field as an empty space instead
+"""
 def join_data_by_area(dataFrame: object, boxDataFrame: object, area: str): 
 
-    try: 
+    filterd_frame = dataFrame[dataFrame['area'].isin([area])];
 
-        filterd_frame = dataFrame[dataFrame['area'].isin([area])];
+    boxDataFrame = boxDataFrame[return_box_label_columns(area=area)];
 
-        boxDataFrame = boxDataFrame[return_box_label_columns(area=area)];
+    joined_frame = filterd_frame.merge(boxDataFrame, on='item_label', how='left').fillna('New Label');
 
-        joined_frame = filterd_frame.merge(boxDataFrame, on='item_label', how='left').fillna('New Label');
-
-        return joined_frame; 
-
-    except: 
-        print("ERROR HAPPENNED AT join_data_by_area METHOD")
-
+    return joined_frame; 
 
 # Grand Summary Files finished; 
 def create_grand_summary(dataFrame: object):
 
-    try: 
+    columns = ['commodity','quantity']
 
-        columns = ['commodity','quantity']
+    dataFrame = dataFrame[columns]\
+                    .groupby(['commodity'], as_index=False)\
+                        .sum().sort_values('commodity'); 
 
-        dataFrame = dataFrame[columns]\
-                        .groupby(['commodity'], as_index=False)\
-                            .sum().sort_values('commodity'); 
+    grand_total = pd.DataFrame({
+        'commodity':['Grand Total'],
+        'quantity': dataFrame['quantity'].sum()
+    })
 
-        grand_total = pd.DataFrame({
-            'commodity':['Grand Total'],
-            'quantity': dataFrame['quantity'].sum()
-        })
+    dataFrame = pd.concat([dataFrame, grand_total])
 
-        dataFrame = pd.concat([dataFrame, grand_total])
-
-        return dataFrame
-
-    except:
-
-        print("ERROR HAPPENNED AT create_grand_summary method")
+    return dataFrame
 
 # Excel File Writer 
-def create_excel_files(detailed_frame: object, grand_sum_frame: object, area):
+def create_excel_files(detailed_frame: object, grand_sum_frame: object, area: str, report_date: str):
 
-    writer = pd.ExcelWriter(f"./data/excel-doc/{area}-Summary.xlsx", engine="openpyxl");
+    writer = pd.ExcelWriter(f"./data/excel-doc/{area}-Summary-{report_date}.xlsx", engine="openpyxl");
 
     grand_sum_frame.to_excel(writer, sheet_name=f"{area}-Grand-Summary", index=False);
 
@@ -263,21 +269,16 @@ def create_excel_files(detailed_frame: object, grand_sum_frame: object, area):
 
 
 # Conditional excel files with detailed and grand summary representation; 
-def condition_excel_files(detailed_frame: object, area_list):
+def condition_excel_files(detailed_frame: object, area_list: str, report_date: str):
 
-    try: 
+    if len(area_list) == 1: 
+        create_excel_files(detailed_frame=detailed_frame[0], grand_sum_frame=create_grand_summary(detailed_frame[0]), area=area_list[0], report_date=report_date)
 
-        if len(area_list) == 1: 
-            create_excel_files(detailed_frame=detailed_frame[0], grand_sum_frame=create_grand_summary(detailed_frame[0]), area=area_list[0])
+    elif len(area_list) == 2: 
+        create_excel_files(detailed_frame=detailed_frame[0], grand_sum_frame=create_grand_summary(detailed_frame[0]), area=area_list[0], report_date=report_date)
+        create_excel_files(detailed_frame=detailed_frame[1], grand_sum_frame=create_grand_summary(detailed_frame[1]), area=area_list[1], report_date=report_date)
 
-        elif len(area_list) == 2: 
-            create_excel_files(detailed_frame=detailed_frame[1], grand_sum_frame=create_grand_summary(detailed_frame[1]), area=area_list[1])
-
-        elif len(area_list) == 3: 
-            create_excel_files(detailed_frame=detailed_frame[0], grand_sum_frame=create_grand_summary(detailed_frame[0]), area=area_list[0])
-            create_excel_files(detailed_frame=detailed_frame[1], grand_sum_frame=create_grand_summary(detailed_frame[1]), area=area_list[1])
-            create_excel_files(detailed_frame=detailed_frame[2], grand_sum_frame=create_grand_summary(detailed_frame[2]), area=area_list[2])
-
-    except: 
-
-        print("ERROR HAPPENNED AT conditional_excel_files METHOD!!!")
+    elif len(area_list) == 3: 
+        create_excel_files(detailed_frame=detailed_frame[0], grand_sum_frame=create_grand_summary(detailed_frame[0]), area=area_list[0], report_date=report_date)
+        create_excel_files(detailed_frame=detailed_frame[1], grand_sum_frame=create_grand_summary(detailed_frame[1]), area=area_list[1], report_date=report_date)
+        create_excel_files(detailed_frame=detailed_frame[2], grand_sum_frame=create_grand_summary(detailed_frame[2]), area=area_list[2], report_date=report_date)
