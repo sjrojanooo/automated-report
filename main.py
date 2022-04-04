24a5db2d42f76582ab6df9d99d2d6379a59ed25d
sjrojanooo <sjrojano@gmail.com>
2022-04-04 08:09:58 -0700

    edit on some comments

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from datetime import datetime # datetime to time the duration of the script; 
 import time; # acts as a timeout function to give time between methods; 
-from scrape_test import scrape_main; # importing my scraping and data manipulation py file
+from scrape_test import scrape_main; # importing my scraping and data manipulation py file;
 from gmail_api import gmail_extract_load # gmail attachment handler to process attachment; 
-from gmail_api import walk_data_excel_dir; 
-from gmail_api import build_message; 
+from gmail_api import build_message; # builds and sends email with attachment; 
 
 
 def main():
 
+    # extracts fetch attachment and download it to the local directory; 
     gmail_extract_load(); 
 
-    # so the newest document in the html directory get read I added a sleep of 5 seconds; 
     time.sleep(3)
 
+    # transforms the data from the document; 
     scrape_main(); 
+
     time.sleep(3);
+    
+    # sends message, could be considered the load phase, since its sending out the scraped document(s)
+    # back out to specific members of the payroll administration; 
     build_message();
 
  
@@ -30,6 +34,6 @@ if __name__ == "__main__":
 
     totalTime = t1 - t2; 
 
-    print(f"Program it took {totalTime}")
+    print(f"Program took {totalTime} to run.")
 
     
